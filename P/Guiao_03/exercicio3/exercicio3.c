d9ac4d103f983c127e3eeb486a70f16443c7a699
Martim Santos <santos.martim@ua.pt>
2023-10-10 13:50:22 +0100

    exercicio3 done

--- P/Guiao_03/exercicio3/exercicio3.c
+++ P/Guiao_03/exercicio3/exercicio3.c
@@ -1,32 +1,43 @@
 #include <stdio.h>
-#include <math.h>
-#include <string.h>
-#include <stblib.h>
 
-int factorion(){
-        int factorial[10];
-        for (int j = 0; j<= 9; j++){
-            factorial[j] = factorial(j);
-        }
+#define MAX 1000000
 
-        for(int i = 0; i <= 1000000; i++){
-            int factorial_sum = 0;
-            
-        
-            char i_string[8];
-
-            sprintf(i_string, "%d", i);
-
-            for (int n; n <= strlength(i_string); n++){
-            int digit = i_string[n] - '0';
-            factorial_sum += factorial[digit];
-
-            }
-
-            if (factorial_sum == i){
-            printf("%d\n", i);
-            }
-        
-        }
+int fatorial(int n) {
+    if (n <= 1) {
+        return 1;
+    }
+    int result = 1;
+    for (int i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
 }
+
+int main() {
+    int fatoriais[10];
+    for (int i = 0; i < 10; i++) {
+        fatoriais[i] = fatorial(i);
+    }
+
+    for (int num = 1; num <= MAX; num++) {
+        int numero = num;
+        int soma_fatoriais = 0;
+
+        while (numero > 0) {
+            int digito = numero % 10;
+            soma_fatoriais += fatoriais[digito];
+            numero /= 10;
+        }
+
+        if (soma_fatoriais == num) {
+            printf("%d\n", num);
+        }
+    }
+
+    return 0;
+}
+
+
+
+
 
